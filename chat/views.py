034a1b238f4c90c504eb82a9574b80f86886041c
Davis Haupt <davis.haupt@gmail.com>
2020-12-22 18:46:15 -0400

    basic functionality

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.shortcuts import render, get_object_or_404
+from chat.models import Room, Message
 
-# Create your views here.
+
+class RoomList(ListView):
+    model = Room
+    context_object_name = 'rooms'
+
+
+class RoomDetail(DetailView):
+    model = Room
+    context_object_name = 'room'
+
+
+class MessageCreate(CreateView):
+    model = Message
+    fields = ["text"]
+
+    def get_success_url(self):
+        return reverse("detail", kwargs={"pk": self.kwargs["pk"]})
+
+    def form_valid(self, form):
+        room = get_object_or_404(Room, pk=self.kwargs["pk"])
+        form.instance.room = room
+        return super().form_valid(form)
+
