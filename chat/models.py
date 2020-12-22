024bdcde71ba5ffcb0f7b93fbab4f86d00e93b8c
Davis Haupt <davis.haupt@gmail.com>
2020-12-22 17:49:27 -0400

    added models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, related_name="messages", on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
